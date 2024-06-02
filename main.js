c6342fc1275a47bdf1f046488786e2ee6735b1d2
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-06-02 21:32:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Class Homework with typescript:-
+// if else conditional statement:-
+let month = "january";
+if (month == "Ramadan") {
+    console.log("food are not allowed"); // false
+}
+else {
+    console.log("food are allowed"); // True
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+              // Class Homework with typescript:-
+// if else conditional statement:-
+let month = "january"
+if (month == "Ramadan"){
+    console.log("food are not allowed")   // false
+}
+else{
+    console.log("food are allowed")       // True
+}
